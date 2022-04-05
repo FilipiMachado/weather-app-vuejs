9238692c609cbb8b44129f99ae0630c0b3b37357
filipi <filipisee@hotmail.com>
2022-04-04 23:29:32 -0300

    adding weather API request

--- src/components/WeatherDetails.vue
+++ src/components/WeatherDetails.vue
@@ -1,21 +1,57 @@
 <template>
   <div class="weatherdetails__main">
     <div class="weatherdetails__container">
-      <span class="weatherdetails__main-title">Nigeria, NG</span>
+      <span class="weatherdetails__main-title">{{ options.params.q }}</span>
       <span class="weatherdetails__subtitle">Sunday 12 January 2020</span>
       <div class="weatherdetails__degrees">
         <span class="weatherdetails__degrees-title">27^c</span>
       </div>
       <span class="weatherdetails__weather-condition">Clouds</span>
     </div>
+    <button @click.prevent="makeRequest">Fazer Request</button>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
   name: "WeatherDetails",
   data() {
-    return {};
+    return {
+      options: {
+        method: "GET",
+        url: "https://community-open-weather-map.p.rapidapi.com/weather",
+        params: {
+          q: "London, UK",
+          lat: "0",
+          lon: "0",
+          callback: "test",
+          id: "2172797",
+          lang: "null",
+          units: "imperial",
+          mode: "xml",
+        },
+        headers: {
+          "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            "25a50a7329mshd6dba41267b3948p1c0c3djsnd3513099cf73",
+        },
+      },
+    };
+  },
+  methods: {
+    makeRequest() {
+      axios
+        .request(this.options)
+        .then(function (response) {
+          console.log(response);
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    },
   },
 };
 </script>
@@ -50,7 +86,7 @@ export default {
 
 .weatherdetails__degrees {
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   padding: 40px 60px;
   margin: 20px;
