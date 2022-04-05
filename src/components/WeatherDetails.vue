787627d8e534aab237e5dadff1472b70ddd99b5d
filipi <filipisee@hotmail.com>
2022-04-04 23:07:46 -0300

    styling components

---
+++ src/components/WeatherDetails.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="weatherdetails__main">
+    <div class="weatherdetails__container">
+      <span class="weatherdetails__main-title">Nigeria, NG</span>
+      <span class="weatherdetails__subtitle">Sunday 12 January 2020</span>
+      <div class="weatherdetails__degrees">
+        <span class="weatherdetails__degrees-title">27^c</span>
+      </div>
+      <span class="weatherdetails__weather-condition">Clouds</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherDetails",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+* {
+  color: #fff;
+}
+
+.weatherdetails__main {
+  display: flex;
+}
+
+.weatherdetails__container {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.weatherdetails__main-title {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.weatherdetails__subtitle {
+  font-size: 18px;
+  font-style: italic;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  margin: 10px;
+}
+
+.weatherdetails__degrees {
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  padding: 40px 60px;
+  margin: 20px;
+  border-radius: 20px;
+}
+
+.weatherdetails__degrees-title {
+  font-size: 48px;
+  font-weight: bold;
+}
+
+.weatherdetails__weather-condition {
+  font-size: 32px;
+  font-weight: bold;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    Home View
+  <div class="main-content">
+    <div class="main-content__header">
+      <SearchBar />
+    </div>
+    <div class="main-content__section">
+      <WeatherDetails />
+    </div>
   </div>
 </template>
 
 <script>
+import SearchBar from "../components/SearchBar.vue";
+import WeatherDetails from "../components/WeatherDetails.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    
-  }
+    SearchBar,
+    WeatherDetails,
+  },
+  data() {
+    return {}
+  },
+  methods: {},
 }
 </script>
+
+<style scoped>
+.main-content {
+  background-image: linear-gradient(to right, #9600FF , #AEBAF8);
+}
+</style>
